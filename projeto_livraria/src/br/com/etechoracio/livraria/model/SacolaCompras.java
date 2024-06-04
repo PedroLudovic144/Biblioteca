233de934f1beb2dfdb95405a44e89122f525d120
PedroLudovic144 <pedroludovic5@gmail.com>
2024-06-04 08:52:01 -0300

    New commit

--- projeto_livraria/src/br/com/etechoracio/livraria/model/SacolaCompras.java
+++ projeto_livraria/src/br/com/etechoracio/livraria/model/SacolaCompras.java
@@ -4,12 +4,14 @@ import java.awt.geom.IllegalPathStateException;
 import java.security.InvalidParameterException;
 
 public class SacolaCompras {
-    private double total;
+   // private double total;
+    private List<Exemplar> itens = new ArrayList<>();
+
     public void adicionar(Exemplar item){
 
         try {
             if (item instanceof Oferta) {
-                ((Oferta) item).aplicarDesconto(new DescontoFixo(-500));
+                ((Oferta) item).aplicarDesconto(new DescontoFixo(1));
             }
         } catch (InvalidParameterException e){
             System.out.println("negativo bb?");
@@ -19,5 +21,13 @@ public class SacolaCompras {
 
         System.out.println("Adicionado " + item);
          total = total + item.getValor();
+         itens.add(item);
     }
+    public void exibirItens(){
+      itens.forEach(i -> System.out.println("Valor:" + i.getValor()));
+    }
+    public double exibirTotal(){
+        return itens.stream().mapToDouble(i -> i.getValor()).sum();
+    }
+
 }
